6933c36c3d2e14258c828e870d01c79c412a9d47
anatoli.anukevich <anatoli.anukevich@exposit.com>
2020-10-19 17:01:16 +0300

    Add WebDriverConfig class

---
+++ src/main/java/com/orangehrmlive/opensourcedemo/opensourcedemouiautomation/config/WebDriverConfig.java
@@ -0,0 +1,58 @@
+package com.orangehrmlive.opensourcedemo.opensourcedemouiautomation.config;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.ie.InternetExplorerOptions;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.context.annotation.Scope;
+
+import static org.springframework.beans.factory.config.ConfigurableBeanFactory.SCOPE_PROTOTYPE;
+
+@Lazy
+@Configuration
+public class WebDriverConfig {
+    @Lazy
+    @Bean
+    @Scope(SCOPE_PROTOTYPE)
+    @ConditionalOnProperty(name = "browser", havingValue = "chrome")
+    @ConditionalOnMissingBean
+    public WebDriver chromeDriver() {
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--start-maximized");
+        return new ChromeDriver(chromeOptions);
+    }
+
+    @Lazy
+    @Bean
+    @Scope(SCOPE_PROTOTYPE)
+    @ConditionalOnProperty(name = "browser", havingValue = "edge")
+    public WebDriver edgeDriver() {
+        WebDriverManager.edgedriver().setup();
+        WebDriver edgeDriver = new EdgeDriver();
+        edgeDriver.manage().window().maximize();
+        return edgeDriver;
+    }
+
+    @Lazy
+    @Bean
+    @Scope(SCOPE_PROTOTYPE)
+    @ConditionalOnProperty(name = "browser", havingValue = "ie")
+    public WebDriver internetExplorerDriver() {
+        WebDriverManager.iedriver().setup();
+        InternetExplorerOptions ieOptions = new InternetExplorerOptions();
+        ieOptions.ignoreZoomSettings();
+        ieOptions.introduceFlakinessByIgnoringSecurityDomains();
+        WebDriver ieDriver = new InternetExplorerDriver(ieOptions);
+        ieDriver.manage().window().maximize();
+        return ieDriver;
+    }
+}
--- src/test/java/com/orangehrmlive/opensourcedemo/opensourcedemouiautomation/base/BaseTest.java
+++ src/test/java/com/orangehrmlive/opensourcedemo/opensourcedemouiautomation/base/BaseTest.java
@@ -3,5 +3,5 @@ package com.orangehrmlive.opensourcedemo.opensourcedemouiautomation.base;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-public class BaseTes {
+public class BaseTest {
 }
