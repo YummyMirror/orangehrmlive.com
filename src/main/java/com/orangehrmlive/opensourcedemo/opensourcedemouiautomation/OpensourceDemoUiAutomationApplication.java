8838d02eff40de2f8b7a41dab45d6ee662c3a952
anatoli.anukevich <anatoli.anukevich@exposit.com>
2020-10-19 16:36:53 +0300

    Add BaseTest class. Change .gitignore-file

---
+++ src/main/java/com/orangehrmlive/opensourcedemo/opensourcedemouiautomation/OpensourceDemoUiAutomationApplication.java
@@ -0,0 +1,11 @@
+package com.orangehrmlive.opensourcedemo.opensourcedemouiautomation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpensourceDemoUiAutomationApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OpensourceDemoUiAutomationApplication.class, args);
+    }
+}
---
+++ src/test/java/com/orangehrmlive/opensourcedemo/opensourcedemouiautomation/base/BaseTes.java
@@ -0,0 +1,7 @@
+package com.orangehrmlive.opensourcedemo.opensourcedemouiautomation.base;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class BaseTes {
+}
