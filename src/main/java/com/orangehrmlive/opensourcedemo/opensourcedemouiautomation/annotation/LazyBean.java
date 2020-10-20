18598ed6683c6ebdb64fee2613ad2a28aca5ed91
anatoli.anukevich <anatoli.anukevich@exposit.com>
2020-10-20 10:01:35 +0300

    Add WebDriverWaitConfig class

---
+++ src/main/java/com/orangehrmlive/opensourcedemo/opensourcedemouiautomation/annotation/LazyBean.java
@@ -0,0 +1,22 @@
+package com.orangehrmlive.opensourcedemo.opensourcedemouiautomation.annotation;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.context.annotation.Scope;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+import static org.springframework.beans.factory.config.ConfigurableBeanFactory.SCOPE_PROTOTYPE;
+
+@Lazy
+@Bean
+@Scope(SCOPE_PROTOTYPE)
+@Documented
+@Target(METHOD)
+@Retention(RUNTIME)
+public @interface LazyBean {
+}
