f68361f30c135d2c0d77346f78f4ff992f90b586
anatoli.anukevich <anatoli.anukevich@exposit.com>
2020-10-21 09:35:11 +0300

    Add 'tearDown' method to BaseTest class

--- src/test/java/com/orangehrmlive/base/BaseTest.java
+++ src/test/java/com/orangehrmlive/base/BaseTest.java
@@ -1,8 +1,19 @@
 package com.orangehrmlive.base;
 
+import com.orangehrmlive.annotation.LazyAutowired;
+import org.openqa.selenium.WebDriver;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.testng.annotations.AfterClass;
 
 @SpringBootTest
 public class BaseTest extends AbstractTestNGSpringContextTests {
+    @LazyAutowired
+    private ApplicationContext context;
+
+    @AfterClass(alwaysRun = true)
+    public void tearDown() {
+        this.context.getBean(WebDriver.class).quit();
+    }
 }
